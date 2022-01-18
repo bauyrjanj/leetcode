b97a1940cc12448c66735474050d364930178d44
benjenis <b.jyenis@gmail.com>
2022-01-17 16:10:28 -0800

    added some easy questions

---
+++ easy/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,25 @@
+def max_profit(arr):
+    profit = 0
+    start = 0
+    end = len(arr)-1
+    min_price = max(arr)
+
+    while start<end:
+        if arr[start]<min_price:
+            min_price = arr[start]
+
+
+
+
+    return profit
+
+
+
+
+if __name__ == "__main__":
+    prices = [7,1,5,3,6,4]
+
+    [1,3,4,5,6,7]
+
+
+
