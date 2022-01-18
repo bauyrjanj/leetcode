8afbe3efb933a4d5b6057a969c140a713433f38b
benjenis <b.jyenis@gmail.com>
2022-01-17 16:34:14 -0800

    updated the solution

--- easy/best_time_to_buy_and_sell_stock.py
+++ easy/best_time_to_buy_and_sell_stock.py
@@ -1,25 +1,19 @@
 def max_profit(arr):
     profit = 0
-    start = 0
-    end = len(arr)-1
     min_price = max(arr)
 
-    while start<end:
-        if arr[start]<min_price:
-            min_price = arr[start]
-
-
-
+    for price in arr:
+        min_price = min(min_price, price)
+        profit = max(profit, price-min_price)
 
     return profit
 
 
-
-
 if __name__ == "__main__":
     prices = [7,1,5,3,6,4]
+    # prices = [7, 6, 4, 3, 1]
 
-    [1,3,4,5,6,7]
+    print(max_profit(prices))
 
 
 
